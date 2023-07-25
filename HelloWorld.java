5b01fbefc8779351005e463b389cdf517475bd1b
TTO <ttoo1@kmd.edu.mm>
2023-07-25 17:00:33 +0630

    july-25_lesson

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,43 @@ package com.example.myapplication;
 public class HelloWorld {
     public static void main(String args[]){
         System.out.println("Hello World");
+        int a = 1;
+        int b = 2;
+        int c = a + b ;
+//        System.out.println(c);
+        System.out.println("a+b="+c );
+
+        boolean a1 = true;
+        boolean b1 = false;
+        System.out.println("a1="+a1);
+        System.out.println("b1="+b1);
+
+//        char x='$';
+//        char y='A';
+//        char z='a';
+//        char m=6;
+//        char q=20;
+//        int w=m+q;
+//        System.out.println(x);
+//        System.out.println(y);
+//        System.out.println(z);
+//        System.out.println(m);
+//        System.out.println(q);
+//        System.out.println(w);
+
+        String str="Hello Android";
+        System.out.println(str);
+
+        float f1=4;
+        float f2=2.0f;
+        double d1=0.05467890;
+        double d2=4333324.34555;
+
+        System.out.println(f1);
+        System.out.println(f2);
+        System.out.println(d1);
+        System.out.println(d2);
+
+
     }
 }
